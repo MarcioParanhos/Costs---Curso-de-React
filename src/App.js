172b718ccd08305db4a15ac1e79d7e8d2d546ef7
MarcioParanhos <marcio.naga@gmail.com>
2023-12-23 00:12:23 -0300

    Proheto do Curso de React - Hora de Codar
    Signed-off-by: MarcioParanhos <marcio.naga@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/pages/home";
+import Company from "./components/pages/company";
+import NewProject from "./components/pages/NewProject";
+import Contact from "./components/pages/Contact";
+import Container from "./components/layout/container";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Routes>
+        <Route path="/" element={<Container><Home /></Container>}/>
+        <Route path="/company" element={<Container><Company /></Container>}/>
+        <Route path="/contact" element={<Container> <Contact /></Container>}/>
+        <Route path="/newproject" element={<Container><NewProject /></Container> } />
+      </Routes>
+      <p>Footer</p>
+    </Router>
   );
 }
 
